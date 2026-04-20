5c064bfafca1249671b6cf1919b928231249a8cd
Babafakruddin Darubhaigari <babadarubhaigari@gmail.com>
2026-04-21 00:49:57 +0530

    addition of two numbers

--- calc/views.py
+++ calc/views.py
@@ -1,7 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
-# Create your views here.
 
 def home(request):
     return render(request, 'home.html', {'name': 'Baba'})
+
+
+def add(request):
+    try:
+        num1 = int(request.GET.get('num1', 0))
+        num2 = int(request.GET.get('num2', 0))
+        result = num1 + num2
+    except ValueError:
+        return render(request, 'result.html', {
+            'result': 'Please enter valid whole numbers.',
+        })
+
+    return render(request, 'result.html', {'result': result})
