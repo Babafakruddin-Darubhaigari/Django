aa69fea14669295bde55640e194c955100198513
Babafakruddin Darubhaigari <babadarubhaigari@gmail.com>
2026-04-21 00:51:00 +0530

    updated code views.py

--- calc/views.py
+++ calc/views.py
@@ -1,17 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+# Create your views here.
 
 def home(request):
     return render(request, 'home.html', {'name': 'Baba'})
 
-
 def add(request):
-    try:
-        num1 = int(request.GET.get('num1', 0))
-        num2 = int(request.GET.get('num2', 0))
-        result = num1 + num2
-    except ValueError:
-        return render(request, 'result.html', {
-            'result': 'Please enter valid whole numbers.',
-        })
-
+    a = int(request.GET['num1'])
+    b = int(request.GET['num2'])
+    result = a + b
     return render(request, 'result.html', {'result': result})
