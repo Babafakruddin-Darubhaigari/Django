ea26550f29e8cfbc7ea9957ffbc633ba14a77f92
Babafakruddin Darubhaigari <babadarubhaigari@gmail.com>
2026-04-21 01:22:40 +0530

    Polish calculator project and docs

--- calc/tests.py
+++ calc/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class CalcViewTests(TestCase):
+    def test_home_page_loads(self):
+        response = self.client.get(reverse('home'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Hello Baba')
+
+    def test_add_view_returns_sum(self):
+        response = self.client.get(reverse('add'), {'num1': 7, 'num2': 5})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Result: 12')
+
+    def test_add_view_handles_invalid_input(self):
+        response = self.client.get(reverse('add'), {'num1': 'abc', 'num2': 5})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Please enter valid whole numbers.')
--- calc/views.py
+++ calc/views.py
@@ -1,13 +1,20 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
-# Create your views here.
 
 def home(request):
     return render(request, 'home.html', {'name': 'Baba'})
 
+
 def add(request):
-    a = int(request.GET['num1'])
-    b = int(request.GET['num2'])
-    result = a + b
+    num1 = request.GET.get('num1')
+    num2 = request.GET.get('num2')
+
+    try:
+        result = int(num1) + int(num2)
+    except (TypeError, ValueError):
+        return render(
+            request,
+            'result.html',
+            {'result': 'Please enter valid whole numbers.'},
+        )
+
     return render(request, 'result.html', {'result': result})
